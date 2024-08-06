421c3833f6df882c9d9c102d936592069ce1753e
danielrobl3s <danielrobles200217@gmail.com>
2024-08-06 16:28:25 -0600

    output for scraped data

--- cour_scraper.py
+++ cour_scraper.py
@@ -16,45 +16,47 @@ def click_modal():
     type_modal = driver.find_element(By.XPATH, '//select[@name="ctl00$ContentPlaceHolder1$cmboType"]')
     type_modal.click()
 
-    time.sleep(4)
+    time.sleep(2)
 
     option_select = driver.find_element(By.XPATH, '//option[@value="Re-Roof"]')
     option_select.click()
 
-    time.sleep(4)
+    time.sleep(2)
 
     submit_btn = driver.find_element(By.XPATH, '//input[@value="Submit Address Search"]')
     submit_btn.click()
 
-    time.sleep(10)
+    time.sleep(2)
 
 
 driver = webdriver.Chrome()
 
 driver.get("https://building.cdaid.org/Permit/Search.aspx/")
 file_name = input('Name your csv file: ')
-name = file_name + '.csv'
 
-time.sleep(4)
+time.sleep(2)
 
 click_modal()
 
 links = len(driver.find_elements(By.XPATH, '//table[@id="ContentPlaceHolder1_grdResults"]//a'))
 
-for link in range(2, links+1):
+with open(f'{file_name}.txt', 'w', encoding='utf-8') as file:
 
-    time.sleep(5)
+    for link in range(2, links+1):
 
-    a = driver.find_element(By.XPATH, '//*[@id="ContentPlaceHolder1_grdResults"]/tbody/tr'+f'[{link}]'+'/td/a')
+        time.sleep(2)
 
-    a.click()
+        a = driver.find_element(By.XPATH, '//*[@id="ContentPlaceHolder1_grdResults"]/tbody/tr'+f'[{link}]'+'/td/a')
 
-    data = driver.find_elements(By.XPATH, '//div[@id="TabStrip-1"]/div')
+        a.click()
 
-    for element in data:
-        print(element.text)
+        data = driver.find_elements(By.XPATH, '//div[@id="TabStrip-1"]/div')
 
-    back = driver.find_element(By.XPATH, '//div[@style="text-align: center;"]/a')
-    back.click()
+        for element in data:
+            print(element.text)
+            file.write(element.text)
 
-    click_modal()
+        back = driver.find_element(By.XPATH, '//div[@style="text-align: center;"]/a')
+        back.click()
+
+        click_modal()
