8ae24ab4b4c3a10016b709312e00dc80bdddb4aa
danielrobl3s <danielrobles200217@gmail.com>
2024-08-06 12:53:40 -0600

    cour_scraper.py browsing through information

--- cour_scraper.py
+++ cour_scraper.py
@@ -2,11 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import nltk
-nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 import csv
 import time
 import re
+
+
+nltk.download('punkt')
+
+#Clicking modal and choosing type of building on Cour D'Alene, Idaho, U.S.
+
+def click_modal():
+    type_modal = driver.find_element(By.XPATH, '//select[@name="ctl00$ContentPlaceHolder1$cmboType"]')
+    type_modal.click()
+
+    time.sleep(4)
+
+    option_select = driver.find_element(By.XPATH, '//option[@value="Re-Roof"]')
+    option_select.click()
+
+    time.sleep(4)
+
+    submit_btn = driver.find_element(By.XPATH, '//input[@value="Submit Address Search"]')
+    submit_btn.click()
+
+    time.sleep(10)
+
 
 driver = webdriver.Chrome()
 
@@ -16,27 +37,24 @@ name = file_name + '.csv'
 
 time.sleep(4)
 
-type_modal = driver.find_element(By.XPATH, '//select[@name="ctl00$ContentPlaceHolder1$cmboType"]')
-type_modal.click()
+click_modal()
 
-time.sleep(4)
+links = len(driver.find_elements(By.XPATH, '//table[@id="ContentPlaceHolder1_grdResults"]//a'))
 
-option_select = driver.find_element(By.XPATH, '//option[@value="Re-Roof"]')
-option_select.click()
+for link in range(2, links+1):
 
-time.sleep(4)
+    time.sleep(5)
 
-submit_btn = driver.find_element(By.XPATH, '//input[@value="Submit Address Search"]')
-submit_btn.click()
+    a = driver.find_element(By.XPATH, '//*[@id="ContentPlaceHolder1_grdResults"]/tbody/tr'+f'[{link}]'+'/td/a')
 
-time.sleep(4)
+    a.click()
 
-headers = driver.find_element(By.XPATH, '//table[@id="ContentPlaceHolder1_grdResults"]//tr[1]')
+    data = driver.find_elements(By.XPATH, '//div[@id="TabStrip-1"]/div')
 
-clean_headers = word_tokenize(headers.text)
+    for element in data:
+        print(element.text)
 
+    back = driver.find_element(By.XPATH, '//div[@style="text-align: center;"]/a')
+    back.click()
 
-""" with open(f'{name}.csv', 'w', encoding='utf-8') as file:
-    fieldnames = ['Title', 'Likes', 'Comments']
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    writer.writeheader() """
+    click_modal()
